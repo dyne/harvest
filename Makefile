73e2ccdd90f2438803baca7c60c50c99f3de421b
Jaromil <jaromil@dyne.org>
2020-05-17 23:17:24 +0200

    cli arg parser, static build and fixed luafilesystem

--- Makefile
+++ Makefile
@@ -1,14 +1,73 @@
 PREFIX ?= /usr/local
+ARCH=$(shell uname -m)
 
-all:
-	make -C file-extension-list/render
+CC ?= gcc
+AR ?= ar
+CFLAGS ?= -O3 --fast-math
+INCLUDE ?= -I src/luajit/src
+LDADD = -lm -ldl
+
+LUA_VERSION ?= luajit-2.1
+LUA_INCLUDE ?= -I /usr/include/$(LUA_VERSION)
+LUA_LDADD ?= /usr/lib/x86_64-linux-gnu/libluajit-5.1.a
+
+MUSL_LDADD ?= /usr/lib/${ARCH}-linux-musl/libc.a
+
+all: devuan-luajit-shared
+
+luajit:
+	make -C src/luajit CC=$(CC) CFLAGS="$(CFLAGS)"
+
+
+luajit-win:
+	make -C src/luajit HOST_CC=gcc CC=$(CC) CFLAGS="$(CFLAGS)" TARGET_SYS=Windows
+
+luastatic:
+	make -C src
+
+# luajit -b src/harvest.lua src/harvest.luac
+# CC="" luastatic src/harvest.luac src/cliargs/*.lua src/lfs.a src/cliargs.lua 
+# rm src/harvest.luac
+
+lfs:
+	$(CC) -c $(LUA_INCLUDE) -O3 --fast-math src/lfs.c -o src/lfs.o
+	$(AR) rcs src/lfs.a src/lfs.o
+
+devuan-luajit-shared: lfs luastatic
+	$(CC) -o harvest $(CFLAGS) $(LUA_INCLUDE) src/harvest.luac.c src/lfs.a $(LUA_LDADD) $(LDADD)
+
+devuan-luajit-static: LUA_LDADD = src/luajit/src/libluajit.a
+devuan-luajit-static: LUA_INCLUDE = -Isrc/luajit/src
+devuan-luajit-static: CC = musl-gcc
+devuan-luajit-static: CFLAGS = -Os -static
+devuan-luajit-static: lfs luastatic luajit
+	$(CC) -static -o harvest $(CFLAGS) $(LUA_INCLUDE) src/harvest.luac.c src/lfs.a $(LUA_LDADD) $(MUSL_LDADD) -static -lm
+
+mingw32-luajit-static: CC=x86_64-w64-mingw32-gcc
+mingw32-luajit-static: AR=x86_64-w64-mingw32-ar
+mingw32-luajit-static: LUA_INCLUDE = -I src/luajit/src
+mingw32-luajit-static: LUA_LDADD = src/luajit/src/libluajit.a
+mingw32-luajit-static: LDLIBS=-lm
+mingw32-luajit-static: lfs luastatic luajit-win
+	$(CC) -static -o harvest.exe $(CFLAGS) $(LUA_INCLUDE) src/harvest.luac.c src/lfs.a $(LUA_LDADD)
+
+win32: mingw32-luajit-static
 
 install:
-	install -d $(PREFIX)/share/harvest/file-extension-list/render/
-	install -p file-extension-list/render/file-extension-parser.zsh \
-      $(PREFIX)/share/harvest/file-extension-list/render/file-extension-parser.zsh
-	install -d $(PREFIX)/share/harvest/zuper/
-	install -p zuper/zuper      $(PREFIX)/share/harvest/zuper/zuper
-	install -p zuper/zuper.init $(PREFIX)/share/harvest/zuper/zuper.init
-	install -p harvest             $(PREFIX)/bin/harvest
+	install -p harvest $(DESTDIR)$(PREFIX)/bin/harvest
+
+clean-luajit:
+	make -C src/luajit clean
+
+clean:
+	rm src/*.o src/*.a
+
+# install:
+# 	install -d $(PREFIX)/share/harvest/file-extension-list/render/
+# 	install -p file-extension-list/render/file-extension-parser.zsh \
+#       $(PREFIX)/share/harvest/file-extension-list/render/file-extension-parser.zsh
+# 	install -d $(PREFIX)/share/harvest/zuper/
+# 	install -p zuper/zuper      $(PREFIX)/share/harvest/zuper/zuper
+# 	install -p zuper/zuper.init $(PREFIX)/share/harvest/zuper/zuper.init
+# 	install -p harvest             $(PREFIX)/bin/harvest
 
--- src/harvest.lua
+++ src/harvest.lua
@@ -20,15 +20,18 @@
 
 local lfs = require'lfs'
 
--- for cli args
--- package.path = package.path ..";"..lfs.currentdir().."/lua_cliargs/src/?.lua"
--- local cli = require'lua_cliargs/src/cliargs'
+-- require local lua libs from src
+package.path = package.path ..";"..lfs.currentdir().."/src/?.lua"
+
+local cli = require'cliargs'
 
 -- debug
 DEBUG=0
 local inspect = require'inspect'
-function I(o) print( inspect.inspect(o) ) end
-function D(s) if DEBUG > 0 then print(s) end end
+local function I(o) print( inspect.inspect(o) ) end
+local function D(s)
+   if DEBUG > 0 then print(s) end
+end
 
 -- # fuzzy thresholds
 -- #
@@ -48,28 +51,11 @@ local fuzzy = {
    sheet=3,
    archiv=10
 }
-local totals = { }
-local scores = { }
-for k, v in pairs(fuzzy) do
-   totals[k] = 0
-   scores[k] = { }
-end
 
 -- https://github.com/dyne/file-extension-list
 local file_extension_list = { ["3dm"] = "image", ["3ds"] = "image", ["3g2"] = "video", ["3gp"] = "video", ["7z"] = "archiv", a = "archiv", aac = "audio", aaf = "video", ai = "image", aiff = "audio", ape = "audio", apk = "archiv", ar = "archiv", asf = "video", au = "audio", avchd = "video", avi = "video", azw = "book", azw1 = "book", azw3 = "book", azw4 = "book", azw6 = "book", bat = "exec", bin = "exec", bmp = "image", bz2 = "archiv", c = "code", cab = "archiv", cbr = "book", cbz = "book", cc = "code", class = "code", clj = "code", command = "exec", cpio = "archiv", cpp = "code", crx = "exec", cs = "code", css = "web", csv = "sheet", cxx = "code", dds = "image", deb = "archiv", diff = "code", dmg = "archiv", doc = "text", docx = "text", drc = "video", dwg = "image", dxf = "image", ebook = "text", egg = "archiv", el = "code", eot = "font", eps = "image", epub = "book", exe = "exec", flac = "audio", flv = "video", gif = "image", go = "code", gpx = "image", gsm = "audio", gz = "archiv", h = "code", html = "code", ics = "sheet", iso = "archiv", it = "audio", jar = "archiv", java = "code", jpeg = "image", jpg = "image", js = "code", kml = "image", kmz = "image", less = "web", lha = "archiv", log = "text", lua = "code", m = "code", m2v = "video", m3u = "audio", m4 = "code", m4a = "audio", m4p = "video", m4v = "video", mar = "archiv", max = "image", md = "text", mid = "audio", mkv = "video", mng = "video", mobi = "book", mod = "audio", mov = "video", mp2 = "video", mp3 = "audio", mp4 = "video", mpa = "audio", mpe = "video", mpeg = "video", mpg = "video", mpv = "video", msg = "text", msi = "exec", mxf = "video", nsv = "video", odp = "slide", ods = "sheet", odt = "text", ogg = "video", ogm = "video", ogv = "video", org = "text", otf = "font", pages = "text", pak = "archiv", patch = "code", pdf = "text", pea = "archiv", php = "code", pl = "code", pls = "audio", png = "image", po = "code", ppt = "slide", ps = "image", psd = "image", py = "code", qt = "video", ra = "audio", rar = "archiv", rb = "code", rm = "video", rmvb = "video", roq = "video", rpm = "archiv", rs = "code", rst = "text", rtf = "text", s3m = "audio", s7z = "archiv", scss = "web", sh = "exec", shar = "archiv", sid = "audio", srt = "video", svg = "image", svi = "video", swift = "code", tar = "archiv", tbz2 = "archiv", tex = "text", tga = "image", tgz = "archiv", thm = "image", tif = "image", tiff = "image", tlz = "archiv", ttf = "font", txt = "text", vb = "code", vcf = "sheet", vcxproj = "code", vob = "video", war = "archiv", wasm = "web", wav = "audio", webm = "video", webp = "image", whl = "archiv", wma = "audio", wmv = "video", woff = "font", woff2 = "font", wpd = "text", wps = "text", xcf = "image", xcodeproj = "code", xls = "sheet", xlsx = "sheet", xm = "audio", xml = "code", xpi = "archiv", xz = "archiv", yuv = "video", zip = "archiv", zipx = "archiv" }
 
-local PWD = arg[1] or lfs.currentdir()
-
-function getextension(p)
-   local i = p:findlast(".", true)
-   if (i) then
-	  return p:sub(i)
-   else
-	  return ""
-   end
-end
-
-function extparser(arg)
+local function extparser(arg)
    local curr = 0
    repeat
 	  local n = arg:find('.',curr+1, true)
@@ -83,6 +69,7 @@ end
 local function analyse_path(basedir, pathname, level)
    local target = pathname or basedir
    local curlev = level or 1
+   local scores = { other = { } }
    -- D("analyse: "..target)
    local path
    for path in lfs.dir(target) do
@@ -91,7 +78,8 @@ local function analyse_path(basedir, pathname, level)
 		 if lfs.attributes(tarpath,"mode") == "directory" then
 			D("found dir:\t"..tarpath.." ("..curlev..")")
 			analyse_path(basedir, tarpath, curlev+1)
-		 else
+
+	 else -- file in subdir
 			local ftype = file_extension_list[ extparser(tarpath) ]
 			if ftype then
 			   if not scores[ftype] then scores[ftype] = { } end
@@ -103,24 +91,109 @@ local function analyse_path(basedir, pathname, level)
 		 end
 	  end
    end
+   return scores
 end
 
-analyse_path(PWD)
-
--- compute a very, very simple linear fuzzy logic for each
-for k,v in pairs(scores) do
-   totals[k] = #v / (fuzzy[k] or fuzzy['other'])
-end
-
-local max = 0
-local guess = 'unknown'
-for k,v in pairs(totals) do
-   if v > max then
-	  max = v
-	  guess = k
+local function fuzzyguess(scores)
+   -- compute a very, very simple linear fuzzy logic for each
+   local res = { guess = 'unknown',
+				 totals = { } }
+   for k,v in pairs(scores) do
+	  res.totals[k] = #v / (fuzzy[k] or fuzzy['other'])
    end
+   local max = 0
+   for k,v in pairs(res.totals) do
+	  if v > max then
+		 max = v
+		 res.guess = k
+	  end
+   end
+   return res
 end
-print''
-print("Path "..PWD.." is "..string.upper(guess))
-print("Fuzzy logic scores:")
-I(totals)
+
+-- CLI: command line argument parsing
+
+cli:set_name("harvest")
+cli:option("-p, --path=PATH", "the path to scan", lfs.currentdir())
+cli:flag("-i, --internal", "analysis of all subdirectories inside the path", false)
+cli:flag("-d", "run in DEBUG mode")
+cli:flag("-v, --version", "print the version and exits", function()
+			print("Harvest version 0.7")
+			os.exit(0) end)
+cli:flag("--score", "print fuzzy logic scores")
+
+cli:command('list', 'List detected categories in the latest scan')
+   :action(function(options)
+		 -- diff implementation goes here
+		 print(cli.name .. " called with: ")
+		 I(options)
+		  end)
+
+-- cli:command('scan', 'List detected categories in the latest scan')
+--    :splat('path', 'Specify which path of those already scanned', nil, 999)
+--    :flag("--score", "print fuzzy logic scores")
+--    :action(function(options)
+-- 		 -- diff implementation goes here
+-- 		 print(cli.name .. " called with: ")
+-- 		 I(options)
+-- 		  end)
+
+local args, err = cli:parse(arg)
+
+if not args and err then
+   -- print(cli.name .. ': command not recognized')
+   print(err)
+   os.exit(1)
+elseif args then -- default command is scan
+   -- recursive
+   local scores
+   local analysis
+   local attr = { }
+   local fileguess
+   local filedate
+   local fattr = lfs.attributes
+   if args.internal then
+	  for file in lfs.dir(args.path) do
+		 if not (file == '.' or file == '..') then
+			-- attr = { size = 0, mode = 'file', modification = os.time() }
+			attr = fattr(file)
+			-- print(file)
+			-- I(attr)
+			if attr.mode == "directory" then   
+			   analysis = fuzzyguess( analyse_path(file) )
+			   collectgarbage'collect'
+			   print("dir,"..analysis.guess..","..attr.modification..",,"..file)
+			else
+			   fileguess = file_extension_list[ extparser(file) ] or 'other'
+			   attr = fattr(file)
+			   print("file,"..fileguess..","..attr.modification..","
+						..attr.size..","..file)
+			end
+		 end
+	  end
+   else
+	  -- external scan: just scan one file or dir
+	  scores = analyse_path(args.path)
+	  analysis = fuzzyguess(scores)
+	  print("Path "..args.path.." is "..string.upper(analysis.guess))
+	  print("Fuzzy logic scores:")
+	  I(analysis.totals)
+   end
+
+end
+
+-- see: https://github.com/amireh/lua_cliargs/blob/master/examples/00_general.lua
+-- if not args and err then
+--    -- something wrong happened and an error was printed
+--    print(string.format('%s: %s; re-run with help for usage', cli.name, err))
+--    os.exit(1)
+-- end
+
+-- local file
+-- for file in lfs.dir(PWD) do
+--    if lfs.attributes(tarpath,"mode") == "directory" then   
+-- analyse_path(PWD)
+
+-- print("Path "..PWD.." is "..string.upper(fuzzyguess()))
+-- print("Fuzzy logic scores:")
+-- I(totals)
